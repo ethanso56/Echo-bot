f26e8055c565cf99e43e857b5924c1087333a4e9
Ethan So <ethan.so.wai.yein@gmail.com>
2020-06-10 22:50:13 +0800

    Add files

---
+++ bot.js
@@ -0,0 +1,44 @@
+const Telegraf = require("telegraf");
+
+const bot = new Telegraf("1208272153:AAEl1bNxLZKkgG0i14SwGwRdTaI8DHLGtWA");
+
+const helpMessage = `
+Say something to me
+/start - start the bot
+/help - command reference
+`;
+
+bot.use((ctx, next) => {
+
+    if (ctx.updateSubTypes[0] == 'text') {
+        bot.telegram.sendMessage(-489429264, ctx.from.username + " said: " + ctx.message.text)
+    } else {
+        bot.telegram.sendMessage(-489429264, ctx.from.username + " sent " + ctx.updateSubTypes[0]);
+    }
+    next();
+})
+
+bot.start((ctx) => {
+    ctx.reply("Hi I am Echo bot");
+    ctx.reply(helpMessage);
+})
+
+bot.help((ctx) => {
+    ctx.reply(helpMessage);
+})
+
+bot.command("echo", (ctx) => {
+    let input = ctx.message.text;
+    let inputArray = input.split(" ");
+    let message = "";
+
+    if (inputArray.length == 1) {
+        message = "You said echo";
+    } else {
+        inputArray.shift();
+        message = inputArray.join(" ");
+    }
+    ctx.reply(message);
+})
+
+bot.launch()
